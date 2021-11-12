7eb84b22001731051ebece2ffa6dd3a68ee03867
Muhammad Rendusara <hadyanrendusara@yahoo.com>
2021-11-12 02:22:33 -0600

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form")
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleToDo(e.target["new-task-description"].value);
+    console.log(e.target["new-task-description"].value);
+    form.reset()
+  })
 });
+
+
+function handleToDo(todo) {
+  let ul = document.querySelector("#tasks");
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+
+//   // btn.addEventListener('click', handleDelete);
+
+  btn.textContent = 'x';
+  li.textContent = `${todo} `;
+
+  
+  li.append(btn);
+  console.log(li);
+
+  ul.append(li);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
